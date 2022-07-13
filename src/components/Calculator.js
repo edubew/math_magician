1a45a612672a4de37687ab3f6f85a6c2a4c05ee7
Edubew <edubew98@gmail.com>
2022-07-14 02:14:25 +0300

    Add  calculate.js and operate.js file to the logic directory and add event listeners

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,51 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export class Calculator extends Component {
-  render() {
-    return (
-      <div className="calculator-grid">
-        <div className="output"><span>0</span></div>
-        <button type="submit">AC</button>
-        <button type="submit">+/-</button>
-        <button type="submit">%</button>
-        <button type="submit" className="orange-btn">÷</button>
-        <button type="submit">7</button>
-        <button type="submit">8</button>
-        <button type="submit">9</button>
-        <button type="submit" className="orange-btn">X</button>
-        <button type="submit">4</button>
-        <button type="submit">5</button>
-        <button type="submit">6</button>
-        <button type="submit" className="orange-btn">-</button>
-        <button type="submit">1</button>
-        <button type="submit">2</button>
-        <button type="submit">3</button>
-        <button type="submit" className="orange-btn">+</button>
-        <button type="submit" className="span-two">0</button>
-        <button type="submit">.</button>
-        <button type="submit" className="orange-btn">=</button>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
   }
+
+  // Event listenter
+    eventHandler = (e) => {
+      const value = e.target.textContent;
+      this.setState((obj) => calculate(obj, value));
+    };
+
+    render() {
+      const { next, total, operation } = this.state;
+      const result = `${total || operation || next ? `${total || ''} ${operation || ''} ${next || ''}` : 0}`;
+      return (
+        <div className="calculator-grid">
+          <div className="output"><span>{result}</span></div>
+          <button type="submit" onClick={this.eventHandler}>AC</button>
+          <button type="submit" onClick={this.eventHandler}>+/-</button>
+          <button type="submit" onClick={this.eventHandler}>%</button>
+          <button type="submit" onClick={this.eventHandler} className="orange-btn">÷</button>
+          <button type="submit" onClick={this.eventHandler}>7</button>
+          <button type="submit" onClick={this.eventHandler}>8</button>
+          <button type="submit" onClick={this.eventHandler}>9</button>
+          <button type="submit" onClick={this.eventHandler} className="orange-btn">x</button>
+          <button type="submit" onClick={this.eventHandler}>4</button>
+          <button type="submit" onClick={this.eventHandler}>5</button>
+          <button type="submit" onClick={this.eventHandler}>6</button>
+          <button type="submit" onClick={this.eventHandler} className="orange-btn">-</button>
+          <button type="submit" onClick={this.eventHandler}>1</button>
+          <button type="submit" onClick={this.eventHandler}>2</button>
+          <button type="submit" onClick={this.eventHandler}>3</button>
+          <button type="submit" onClick={this.eventHandler} className="orange-btn">+</button>
+          <button type="submit" onClick={this.eventHandler} className="span-two">0</button>
+          <button type="submit" onClick={this.eventHandler}>.</button>
+          <button type="submit" onClick={this.eventHandler} className="orange-btn">=</button>
+        </div>
+      );
+    }
 }
 
 export default Calculator;
