fa3bf5b490e05168a54cc068a76816c26a5988b9
Edubew <edubew98@gmail.com>
2022-07-13 05:26:46 +0300

    Fix linters errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export class App extends Component {
   render() {
     return (
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export class Calculator extends Component {
   render() {
     return (
